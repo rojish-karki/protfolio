0dc33a4b4ed5612a495be94a1c6f996fdca196f0
rojish-karki <info.xaritoo@gmail.com>
2026-02-02 04:14:50 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+// MOBILE MENU
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
+// SCROLL REVEAL
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      }
+    });
+  },
+  { threshold: 0.15 }
+);
+
+reveals.forEach(el => observer.observe(el));
